a1ed0879fc640b612b45d9f04dde3b408697de69
nika nozadze <nikanozadze668@gmail.com>
2023-05-21 17:01:14 +0400

    CalculatorWPF
    CalculatorWPF

---
+++ 2_Calculator/2_Calculator/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace _2_Calculator
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			foreach (UIElement el in MainRoot.Children)
+			{
+				if (el is Button)
+				{
+					((Button)el).Click += Button_Click;
+				}
+			}
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			//OriginalSource - anu originals viyenebt. Content - mis kontents viyenebt
+			string str = (string)((Button)e.OriginalSource).Content;
+
+			try
+			{
+				if (str == "AC")
+				{
+					TextLabel.Text = string.Empty;
+				}
+				else if (str == "=")
+				{
+					string? value = new DataTable().Compute(TextLabel.Text, null).ToString();
+					TextLabel.Text = value;
+				}
+				else
+				{
+					//cifrebi gverdigverd rom daiweros
+					TextLabel.Text += str;
+				}
+			}
+			catch
+			{
+				TextLabel.Text = "ERROR";
+			}
+		}
+	}
+}
